98c80e12154c70247bb3ebc821f91b4f7c93d992
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2022-10-16 13:39:19 +0700

    commit typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
     <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
       <div class="row justify-content-center">
         <div class="col-xl-7 col-lg-9 text-center">
-          <h1>Undangan Aniversery 4th Ultras Garuda Sezione Tangerang</h1>
+          <h1>Undangan Anniversary 4th Ultras Garuda Sezione Tangerang</h1>
           <h2></h2>
         </div>
       </div>
