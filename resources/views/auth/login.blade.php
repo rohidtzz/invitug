1819d045ac51f7d68ede038b243d0e08f1ed9996
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2022-10-17 22:04:55 +0700

    commit ico auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="{{ asset('ico/ico.ico') }}"/>
+	<link rel="icon" type="image/png" href="{{ asset('ico/tgr.ico') }}"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('loginn/vendor/bootstrap/css/bootstrap.min.css') }}">
 <!--===============================================================================================-->
